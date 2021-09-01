9add1ec2e37bae3def29d51ed80d0d835028186d
miguelapabenedit <miguell.beneditt@gmail.com>
2021-09-01 15:53:40 -0300

    refactor

--- app/routes.go
+++ app/routes.go
@@ -16,7 +16,7 @@ var controllers controller.Controllers = controller.NewController(serv)
 
 func SetUpPublicRoutes(rootPath string, r *mux.Router) {
 	r.HandleFunc(fmt.Sprintf("%s/user", rootPath), controllers.CreateUserHandler).Methods(http.MethodPost)
-	r.HandleFunc(fmt.Sprintf("%s/healthCheck", rootPath), controllers.HealthCheckHandler).Methods(http.MethodGet)
-
 	r.HandleFunc(fmt.Sprintf("%s/user", rootPath), controllers.GetUserHandler).Methods(http.MethodGet)
+
+	r.HandleFunc(fmt.Sprintf("%s/healthCheck", rootPath), controllers.HealthCheckHandler).Methods(http.MethodGet)
 }
