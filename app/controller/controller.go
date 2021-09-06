db7cadee4114cb5ae786d3941cb400eed64cc609
miguelapabenedit <miguell.beneditt@gmail.com>
2021-09-06 20:17:18 -0300

    add cors

--- app/controller/controller.go
+++ app/controller/controller.go
@@ -38,6 +38,10 @@ func (c *controllers) HealthCheckHandler(w http.ResponseWriter, r *http.Request)
 }
 
 func (c *controllers) GetUserHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Access-Control-Allow-Origin", "*")
+	w.Header().Add("Access-Control-Allow-Credentials", "true")
+	w.Header().Add("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+	w.Header().Add("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 
 	id := r.URL.Query().Get("id")
 	if len(id) < 1 {
