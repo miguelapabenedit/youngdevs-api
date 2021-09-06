bc1fa7339e3d908e8d7f162dafb14ad86eb93882
miguelapabenedit <miguell.beneditt@gmail.com>
2021-09-06 20:51:03 -0300

    add changes

--- app/controller/controller.go
+++ app/controller/controller.go
@@ -29,7 +29,6 @@ func NewController(service service.Service) Controllers {
 func (c *controllers) HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Access-Control-Allow-Origin", "*")
 	w.Header().Add("Access-Control-Allow-Credentials", "true")
-	w.Header().Add("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 	w.Header().Add("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 
 	msg, err := json.Marshal("Im alive Belen")
@@ -45,7 +44,6 @@ func (c *controllers) HealthCheckHandler(w http.ResponseWriter, r *http.Request)
 func (c *controllers) GetUserHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Access-Control-Allow-Origin", "*")
 	w.Header().Add("Access-Control-Allow-Credentials", "true")
-	w.Header().Add("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 	w.Header().Add("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 
 	id := r.URL.Query().Get("id")
@@ -69,7 +67,6 @@ func (c *controllers) GetUserHandler(w http.ResponseWriter, r *http.Request) {
 func (c *controllers) CreateUserHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Access-Control-Allow-Origin", "*")
 	w.Header().Add("Access-Control-Allow-Credentials", "true")
-	w.Header().Add("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 	w.Header().Add("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 
 	user := &entity.User{}
