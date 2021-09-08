60c9b1d63dcbac608c890121271f3acc43e9ea98
miguelapabenedit <miguell.beneditt@gmail.com>
2021-09-07 22:27:38 -0300

    change id mock

--- app/infrastructure/user.go
+++ app/infrastructure/user.go
@@ -3,13 +3,13 @@ package infrastructure
 import "github/miguelapabenedit/youngdevs-api/app/data"
 
 func CreateUser(u *data.User) error {
-	(u).ID = "idgenerated"
+	(u).ID = "IDGeneratedYoungdevsApi:" + u.ID
 	return nil
 }
 
 func GetUser(id string) *data.User {
 	var user data.User
-	user.ID = id
+	user.ID = "IDGeneratedYoungdevsApi:" + id
 
 	return &user
 }
